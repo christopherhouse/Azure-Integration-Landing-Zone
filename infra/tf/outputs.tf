68ec7d92068cd8b65581c106bc5d1adc647febde
Chris House <chhouse@microsoft.com>
2025-06-02 09:53:24 -0500

    updated outputs

--- infra/tf/outputs.tf
+++ infra/tf/outputs.tf
@@ -1,4 +1,7 @@
-output "log_analytics_workspace_id" {
-  value = module.log_analytics.workspace_id
+output "apim_private_ip" {
+  value = module.apim.private_ip
 }
 
+output "azure_firewall_private_ip" {
+  value = module.azure_firewall.private_ip
+}
